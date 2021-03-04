597b91353ed3958ce39103ce33cf9b54833c48c6
JonathanRome <jhoropertuz@gmail.com>
2021-03-04 15:58:26 -0500

    modificaciones en vistas programadas

--- src/app/endpoind.service.ts
+++ src/app/endpoind.service.ts
@@ -9,10 +9,9 @@ export class EndpoindService {
   getEndpoindGov():string{
     //let uri = 'http://172.16.19.25:3000';
     //uri = 'http://localhost:3000';
-
     //let uri='https://desarrollosicam32-net-jpllinas943510.codeanyapp.com/api/';
     let uri='https://api.sicam32.net/';
-
+    //let uri='https://api.sicam32.net.co/';
     return uri;
   }
 }
